de5050bcdcfc2dd93b424c5736990f3b64e78de6
matrix <113445886@qq.com>
2022-04-01 10:38:38 +0800

    chore: export repository's url

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { ecrRepository } from "./ecr";
 import { hasuraEngineRds, hasuraMetadataRds } from "./rds";
 
 export const hasuraEngineRepositoryName = ecrRepository.name;
+export const hasuraEngineRepositoryUrl = ecrRepository.repositoryUrl;
 
 export const hasuraEngineDatabaseName = hasuraEngineRds.dbName;
 export const hasuraEngineDatabaseAddress = hasuraEngineRds.address;
